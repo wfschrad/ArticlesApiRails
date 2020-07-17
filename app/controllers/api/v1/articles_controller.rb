004b11142cda6817703fb33b6babec5f6509d4bc
William Schrader <wfschrad@gmail.com>
2020-07-17 15:21:21 -0700

    Add 'DELETE articles/:id' and 'PUT articles/:id'.

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -36,6 +36,33 @@ module Api
                 end
             end
 
+            def destroy
+                article = Article.find(params[:id])
+                article.destroy
+                render json: {
+                    status: 'SUCCESS',
+                    message: 'deleted article',
+                    data: article
+                    }, status: :ok
+            end
+
+            def update
+                article = Article.find(params[:id])
+                if article.update_attributes(article_params)
+                    render json: {
+                        status: 'SUCCESS',
+                        message: 'updated article',
+                        data: article
+                        }, status: :ok
+                else
+                    render json: {
+                        status: 'ERROR',
+                        message: 'Article not updated',
+                        data: article.errors
+                        }, status: :unprocessable_entity 
+                end
+            end
+
             private
 
             def article_params
