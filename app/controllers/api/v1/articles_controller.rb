3653f77707ca45e860deb73d881e9d7f8e5c0056
William Schrader <wfschrad@gmail.com>
2020-07-17 15:16:02 -0700

    Add 'GET articles/:id' and 'POST articles'.

--- app/controllers/api/v1/articles_controller.rb
+++ app/controllers/api/v1/articles_controller.rb
@@ -9,6 +9,38 @@ module Api
                     data: articles
                 }, status: :ok
             end
+
+            def show
+                article = Article.find(params[:id])
+                render json: {
+                    status: 'SUCCESS',
+                    message: 'Loaded article',
+                    data: article
+                }, status: :ok
+            end
+
+            def create
+                article = Article.new(article_params)
+                if article.save
+                    render json: {
+                    status: 'SUCCESS',
+                    message: 'Saved article',
+                    data: article
+                    }, status: :ok
+                else
+                    render json: {
+                    status: 'ERROR',
+                    message: 'Article not saved',
+                    data: article.errors
+                    }, status: :unprocessable_entity 
+                end
+            end
+
+            private
+
+            def article_params
+                params.permit(:title, :body)
+            end
         end
     end
 end
